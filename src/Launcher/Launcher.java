55d4275f9548a8bf3bda5879cc39bcd1bede512e
qvpiotr <piotrek.witek1@gmail.com>
2020-12-05 00:31:58 +0100

    add start, previous production, next production buttons

--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -16,7 +16,7 @@ import javax.swing.*;
 public class Launcher extends JFrame{
 
     public ArrayList<Stats> stats;
-    boolean next = false;
+    int i=0;
 
     public void launchFile() throws IOException {
         LauncherAssistant assistant = new LauncherAssistant();
@@ -36,19 +36,19 @@ public class Launcher extends JFrame{
 
         // poszczegolne statystyki
         JLabel label1 = new JLabel();
-        label1.setText(String.valueOf("Produkcja: " + i));
+        label1.setText(String.valueOf("Ostatnio wykonana produkcja: " + i));
         JLabel label2 = new JLabel();
-        label2.setText(String.valueOf("Ilość wierzchołków: " + stat.vertexCounter()));
+        label2.setText(String.valueOf("Ilosc krawedzi: " + stat.edgeCounter()));
         JLabel label3 = new JLabel();
-        label3.setText(String.valueOf("Ilość krawędzi: " + stat.edgeCounter()));
+        label3.setText(String.valueOf("Ilosc silnie spojnych skaldowych: " + stat.stronglyConnectedComponents()));
         JLabel label4 = new JLabel();
-        label4.setText(String.valueOf("Ilość silnie spójnych składowych: " + stat.stronglyConnectedComponents()));
+        label4.setText(String.valueOf("Ilosc wierzchołkow: " + stat.vertexCounter()));
         JLabel label5 = new JLabel();
         label5.setText(String.valueOf("Średni stopień wierzchołka: " + stat.averageDegree()));
         JLabel label6 = new JLabel();
-        label6.setText(String.valueOf("Średni stopień wierzchołka {a,b,c,d}: " + stat.averageVDegree()));
+        label6.setText(String.valueOf("Średni stopień wierzchołka: " + stat.averageVDegree()));
         JLabel label7 = new JLabel();
-        label7.setText(String.valueOf("Średnia liczba węzłów w składowej spójnej: " + stat.averageNumberSCC()));
+        label7.setText(String.valueOf("Średni stopień wierzchołka: " + stat.averageNumberSCC()));
 
         statistics.add(label1);
         statistics.add(label2);
@@ -74,44 +74,50 @@ public class Launcher extends JFrame{
         this.setVisible(true);
         this.setResizable(false);
 
-//        Box itemBox = Box.createHorizontalBox();
+        Box itemBox = Box.createHorizontalBox();
 
-        JButton button = new JButton("Następna produkcja");
+        JButton button1 = new JButton("Start");
+        JButton button2 = new JButton("Poprzednia produkcja");
+        JButton button3 = new JButton("Następna produkcja");
 
-//        itemBox.add(button);
+        itemBox.add(button1);
+        itemBox.add(button2);
+        itemBox.add(button3);
 
-
-        c.add(button,BorderLayout.NORTH);
+        c.add(itemBox,BorderLayout.NORTH);
 
         setTitle("Transformacje grafowe");
         setSize(750, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-
-        button.addActionListener(new ActionListener() {
+        button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                next = true;
+                buildGUI(stats.get(0),1,c);
+            }
+        });
+        //dodac obsluge wyjątków
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(i-1>=0){
+                    i-=1;
+                    buildGUI(stats.get(i),i+1,c);
+                }
 
             }
         });
-
-        // kazda iteracja to kolejna produkcja
-        for(int i = 0; i < stats.size(); i++ ) {
-            next = false;
-
-            buildGUI(stats.get(i), i+1,c);
-
-            while(!next) {
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
+        //dodac obsluge wyjątków
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(i+1<stats.size()){
+                    i+=1;
+                    buildGUI(stats.get(i),i+1,c);
                 }
             }
-
-        }
+        });
 
     }
 
